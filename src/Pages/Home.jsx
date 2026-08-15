16d6870e6d670c0b85130649615b3600e5fcb0fa
FarazKTK <khattakfaraz02@gmail.com>
2026-08-16 00:54:06 +0500

    Resolve all merge conflicts

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import Header from '../Components/Header'
 import Hero from '../Components/Hero'
@@ -26,33 +25,4 @@ const Home = () => {
   )
 }
 
-=======
-import React from 'react'
-import Header from '../Components/Header'
-import Hero from '../Components/Hero'
-import Cursor from '../Components/Cursor'
-import Marque from '../Components/Marque'
-import Collection from '../Components/Collection'
-import Arrival from '../Components/Arrival'
-import About from '../Components/About'
-import Contact from '../Components/Contact'
-import Footer from '../Components/Footer'
-
-const Home = () => {
-  return (
-    <>
-        <Header />
-        <Hero />
-        <Cursor />
-        <Marque />
-        <Collection />
-        <Arrival />
-        <About />
-        <Contact />
-        <Footer />
-    </>
-  )
-}
-
->>>>>>> 218ab3f7061f0852bbf6f7278afb7cda83ecbf6f
 export default Home
